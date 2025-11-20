22a6d841fbb65f2b14a0ae5a44449a86f2662f7b
Heeme Beeme <heemebeeme@gmail.com>
2025-11-20 17:42:45 -0400

    public player settings changed

--- Assets/scripts/FirstPersonController.cs
+++ Assets/scripts/FirstPersonController.cs
@@ -35,12 +35,12 @@ public class FirstPersonController : MonoBehaviour
 
     [Header("Movement Speeds")]
     public float WalkSpeed = 3f;
-    public float RunSpeed = 5f;
+    public float RunSpeed = 6f;
     public float CrouchSpeed = 1f;
-    public float SpeedIntensityChange = 4;
+    public float SpeedIntensityChange = 6;
 
     [Header("Other Useful Settings")]
-    public float MouseSensitivity = 2f;
+    public float MouseSensitivity = 1f;
     public float JumpIntensity = 4;
 
 
